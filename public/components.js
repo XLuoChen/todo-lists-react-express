7929cc080754ba678ec24e9040e8b71ae43996a0
XLuoChen <1425145741@qq.com>
2016-08-07 01:03:36 +0800

    add item

--- public/components.js
+++ public/components.js
@@ -1,15 +1,33 @@
 const App = React.createClass({
+  getInitialState(){
+    return {
+      items: []
+    }
+  },
+  addItem: function (item) {
+    this.state.items.push({item: item, status: 'active'});
+    this.setState({items: this.state.items});
+  },
   render: function () {
     return <div>
-      <Header />
+      <Header addItem={this.addItem}/>
       <Footer />
     </div>
   }
 });
 
 const Header = React.createClass({
+  addItem: function (e) {
+    if (e.keyCode === 13) {
+      const item = e.target.value;
+      e.target.value = '';
+      this.props.addItem(item);
+    }
+  },
   render: function () {
-    return <div>Header</div>
+    return <div>
+      <input type="text" name="input" onKeyUp={this.addItem}/>
+    </div>
   }
 });
 
@@ -17,7 +35,10 @@ const Footer = React.createClass({
   render: function () {
     return <div>
       <ItemsList />
-      <button>button</button>
+      <button>all</button>
+      <button>active</button>
+      <button>completed</button>
+      <button>clear all</button>
     </div>
   }
 });
